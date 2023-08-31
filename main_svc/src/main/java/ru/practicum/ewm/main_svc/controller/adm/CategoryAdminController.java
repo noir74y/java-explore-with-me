f8c42f29e572723e1e4fd81551b6b833ba51b7f5
noir74y <noir74@yandex.ru>
2023-09-01 01:40:15 +0300

    feat: to add templates of controllers

---
+++ main_svc/src/main/java/ru/practicum/ewm/main_svc/controller/adm/CategoryAdminController.java
@@ -0,0 +1,4 @@
+package ru.practicum.ewm.main_svc.controller.adm;
+
+public class CategoryAdminController {
+}
---
+++ main_svc/src/main/java/ru/practicum/ewm/main_svc/controller/pub/CompilationPublicController.java
@@ -0,0 +1,4 @@
+package ru.practicum.ewm.main_svc.controller.pub;
+
+public class CompilationPublicController {
+}

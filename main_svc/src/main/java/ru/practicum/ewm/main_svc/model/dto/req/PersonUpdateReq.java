04dc7801f107bd5b590f5e11b1b77696c73663d2
noir74y <noir74@yandex.ru>
2023-09-01 01:12:52 +0300

    feat: to add templates of dto classes

---
+++ main_svc/src/main/java/ru/practicum/ewm/main_svc/model/dto/req/PersonUpdateReq.java
@@ -0,0 +1,4 @@
+package ru.practicum.ewm.main_svc.model.dto.req;
+
+public class PersonUpdateReq {
+}
---
+++ main_svc/src/main/java/ru/practicum/ewm/main_svc/model/dto/resp/CategoryResp.java
@@ -0,0 +1,4 @@
+package ru.practicum.ewm.main_svc.model.dto.resp;
+
+public class CategoryResp {
+}

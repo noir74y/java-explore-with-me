d675669b9b74a00c9a868d3934f9f5f197f82779
noir74y <noir74@yandex.ru>
2023-09-03 23:25:35 +0300

    feat: to add paging to UserServiceImpl

--- main_svc/src/main/java/ru/practicum/ewm/main_svc/repository/UserRepository.java
+++ main_svc/src/main/java/ru/practicum/ewm/main_svc/repository/UserRepository.java
@@ -1,15 +1,11 @@
 package ru.practicum.ewm.main_svc.repository;
 
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import ru.practicum.ewm.main_svc.model.dto.UserDto;
 import ru.practicum.ewm.main_svc.model.entity.User;
-
-import java.util.List;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    Iterable<User> findByIdIn(Iterable<Long> ids, Pageable pageable);
+    Iterable<User> findByIdIn(Iterable<Long> ids, PageRequest pageRequest);
 }
